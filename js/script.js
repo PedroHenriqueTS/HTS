fce9ad06248e86e80ea9b3ea14a6b7d4dcbd2458
PedroHenriqueTS <124845964+PedroHenriqueTS@users.noreply.github.com>
2023-06-14 21:50:16 -0300

    script.js

--- js/script.js
+++ js/script.js
@@ -58,3 +58,65 @@ const typed = new Typed('.multiplo-texto', {
     backDelay: 1000,
     loop: true
 });
+
+
+
+/*-------------------------SEND MESSAGE WITH TELEGRAM ----------------------------*/
+// Função para enviar a mensagem para o canal do Telegram
+async function enviarMensagemTelegram(mensagem) {
+   const token = '6063001127:AAFUIYm7NvEiMo59Q0TzU2t6ZoFmspHn8BY'; // Substitua pelo seu token do bot do Telegram
+  const chatId = '6042061963'; // Substitua pelo ID do canal
+
+
+  try {
+    const response = await fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        chat_id: chatId,
+        text: mensagem
+      })
+    });
+
+    if (response.ok) {
+      alert('Mensagem enviada com sucesso para o Telegram!');
+      limparFormulario();
+    } else {
+      console.error('Erro ao enviar mensagem para o Telegram:', response.status);
+    }
+  } catch (error) {
+    console.error('Erro ao enviar mensagem para o Telegram:', error);
+  }
+}
+
+// Função para limpar os campos do formulário
+function limparFormulario() {
+  document.getElementById('nome').value = '';
+  document.getElementById('email').value = '';
+  document.getElementById('mensagem').value = '';
+  document.getElementById('assunto').value = '';
+  document.getElementById('telefone').value = '';
+}
+
+// Função para lidar com o envio do formulário
+function enviarFormulario(event) {
+  event.preventDefault(); // Impede o comportamento padrão de envio do formulário
+
+  // Obtém os valores do formulário
+  const nome = document.getElementById('nome').value;
+  const assunto = document.getElementById('assunto').value;
+  const telefone = document.getElementById('telefone').value;
+  const email = document.getElementById('email').value;
+  const mensagem = document.getElementById('mensagem').value;
+
+  // Monta a mensagem a ser enviada
+  const mensagemTelegram = `Nome: ${nome}\nTelefone: ${telefone}\nAssunto: ${assunto}\nEmail: ${email}\nMensagem: ${mensagem}`;
+
+  // Envia a mensagem para o Telegram
+  enviarMensagemTelegram(mensagemTelegram);
+}
+
+// Adiciona um listener de evento para o envio do formulário
+document.getElementById('formulario').addEventListener('submit', enviarFormulario);
